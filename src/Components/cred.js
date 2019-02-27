71d8b1717751830464beb1bb7362c89d7a6f96ba
Adam Trincas <atrincas@gmail.com>
2019-02-27 09:47:49 +0100

    applicationId and secret removed

--- src/Components/cred.js
+++ src/Components/cred.js
@@ -1,8 +1,8 @@
 import Unsplash from 'unsplash-js';
 
 const cred = new Unsplash({
-	applicationId : "3aef38df941b3148d09e93479a18cbac8f3e906b38e3c24225bc915c48f9c849",
-	secret : "71e1dadb20924db6814c7368163140a69466fc2094781fa5c4f6865163ab73c2"
+	applicationId : "",
+	secret : ""
 });
 
 export default cred;
